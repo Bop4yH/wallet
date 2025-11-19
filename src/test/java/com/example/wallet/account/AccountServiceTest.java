45344e9818a0d60dacc3d363f1eb8fdf473f0acb
Кузьмин Александр Владимирович <al.kuzmin@korona.net>
2025-11-19 16:06:34 +0700

    feat: add unit test for com.example.wallet.account.AccountService.create

---
+++ src/test/java/com/example/wallet/account/AccountServiceTest.java
@@ -0,0 +1,123 @@
+package com.example.wallet.account;
+
+import com.example.wallet.account.dto.AccountResponse;
+import com.example.wallet.common.MoneyConstants;
+import com.example.wallet.transfer.TransferRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AccountServiceTest {
+
+
+    private static final BigDecimal EMPTY_BALLANCE = money(0);
+
+    private static final BigDecimal DEFAULT_BALANCE = money(100);
+
+    private static final UUID DEFAULT_ACCOUNT_ID = new UUID(69, 69);
+
+    private static final OffsetDateTime FIXED_TIME =
+            OffsetDateTime.parse("2025-01-01T12:00:00Z");
+
+    @Mock
+    private AccountRepository accountRepo;
+
+    @Mock
+    private TransferRepository transferRepo;
+
+    @InjectMocks
+    private AccountService accountService;
+
+    @Captor
+    private ArgumentCaptor<Account> accountCaptor;
+
+    // ==================== HELPER METHODS ====================
+
+    private Account savedAccount(String owner, String currency, long balance) {
+        return Account.builder()
+                .id(DEFAULT_ACCOUNT_ID)
+                .ownerName(owner)
+                .currency(currency)
+                .balance(money(balance))
+                .createdAt(FIXED_TIME)
+                .build();
+    }
+
+    private Account defaultAccount() {
+        return savedAccount("John", "USD", 100);
+    }
+
+    private Account savedEmptyAccount() {
+        return savedAccount("John", "USD", 0);
+    }
+
+    private static final BigDecimal money(long balance){
+        return BigDecimal.valueOf(balance).setScale(
+                MoneyConstants.SCALE,
+                RoundingMode.HALF_UP
+        );
+    }
+
+    // ==================== CREATE ====================
+
+    @Test
+    void create_success() {
+        Account saved = savedEmptyAccount();
+        when(accountRepo.save(any())).thenReturn(saved);
+
+        AccountResponse response = accountService.create("John", "USD");
+
+        verify(accountRepo).save(accountCaptor.capture());
+        assertEquals("John", response.getOwnerName());
+        assertEquals("USD", response.getCurrency());
+        assertEquals(EMPTY_BALLANCE, response.getBalance());
+        assertEquals(DEFAULT_ACCOUNT_ID, response.getId());
+        assertEquals(FIXED_TIME, response.getCreatedAt());
+
+        Account captured = accountCaptor.getValue();
+        assertEquals("John", captured.getOwnerName());
+        assertEquals("USD", captured.getCurrency());
+        assertNull(captured.getBalance());
+        assertNull(captured.getId());
+        assertNull(captured.getCreatedAt());
+    }
+
+    @Test
+    void create_duplicateAccount() {
+        when(accountRepo.save(any()))
+                .thenThrow(new DataIntegrityViolationException("😎😎😎"));
+
+        ResponseStatusException ex = assertThrows(
+                ResponseStatusException.class,
+                () -> accountService.create("John", "USD")
+        );
+
+        assertEquals(HttpStatus.CONFLICT, ex.getStatusCode());
+
+        assertThat(ex.getReason())
+                .contains("already exists", "John", "USD");
+    }
+
+
+}
